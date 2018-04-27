0130a5f354197defbfc7db58607f4a9a808e581a
github username <github email address>
2018-04-27 19:37:21 -0400

    create index route in books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,14 @@
 class BooksController < ApplicationController
 
+  #get user's books
+  get '/books' do
+    if logged_in?
+      @books = Book.all
+      erb :'/books/books_list'
+    else
+      erb :'login'
+    end
+
+  end
+
 end
