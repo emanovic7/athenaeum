001acadd9941b13e00f14c22c2522aa5aa52db2c
github username <github email address>
2018-04-27 15:44:56 -0400

    remove 'html' from view file names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions
     set :session_secret, "athenaeum_secret"
   end
