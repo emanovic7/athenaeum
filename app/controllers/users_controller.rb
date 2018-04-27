cf1d4dfff12a43c0bef6cd4902fa927f5812d325
github username <github email address>
2018-04-27 17:59:56 -0400

    add routes to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,61 @@
 class UsersController < ApplicationController
 
+  get '/register' do
+
+     erb :'/users/register'
+ end
+
+ post '/register' do
+     if params[:user_name] == "" || params[:email] == "" || params[:password_digest] == ""
+       redirect to '/register'
+     else
+       @user=User.create(user_name: params[:user_name], email: params[:email], password: params[:password_digest])
+       session[:user_id] = @user.id
+       erb :'/users/personal'
+     end
+ end
+
+
+ get '/login' do
+   if !logged_in?
+     erb :'/users/login'
+   else
+     redirect to '/books/books_list'
+   end
+ end
+
+ post '/login' do
+   @user = User.find_by(user_name: params[:user_name])
+     if @user !=nil && @user.password == params[:password]
+       session[:user_id] = @user.id
+       redirect to '/books/books_list'
+     else
+       redirect to '/login'
+     end
+ end
+
+
+
+ get '/logout' do
+   if logged_in?
+     session.destroy
+     redirect to '/'
+   else
+     redirect to '/login'
+   end
+ end
+
+ #list user's books' subjects
+
+ get '/users/books/subjects' do
+   if logged_in?
+     @subjects = Subject.all #make subjects controller?
+     erb :'/subjects/subjects'
+   else
+     redirect to '/login'
+   end
+ end
+
 
 
 end
