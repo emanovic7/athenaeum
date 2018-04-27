32e4da1dc66604f9b2cfe6713b1c72dc3a57cfb5
github username <github email address>
2018-04-27 19:38:31 -0400

    add get/books/new, post/books/new to books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,31 @@ class BooksController < ApplicationController
     else
       erb :'login'
     end
+  end
 
+  #add new book
+  get '/books/new' do
+    if logged_in?
+      erb :'books/add_book'
+    else
+      redirect to '/login'
+    end
+  end
+
+#process submitted book info
+  post '/books/new' do
+    if logged_in?
+      @book = Book.create(name: params["book title"])
+      @book.author = Author.find_or_create_by(name: params["author name"])
+      @book.subject_ids = params[:subjects]
+      @book.save
+
+      flash[:message] = "New Book Added!"
+
+      erb :'/books/show_book'
+    else
+      redirect to '/login'
+    end
   end
 
 end
