74225f7176ef0d1f202b5d11338b0b9837b4ac9e
github username <github email address>
2018-04-28 12:26:49 -0400

    add subject/all, subject/:slug to subjects_controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,4 +1,20 @@
 class SubjectsController < ApplicationController
 
+  #show all subjects
+  get '/subjects' do
+   if logged_in?
+     @subjects = Subject.all
+     erb :'subjects/subjects'
+   else
+     redirect to '/login'
+   end
+ end
+
+ #show subject by slug
+ get '/subjects/:slug' do
+   @subject = Subject.find_by_slug(params[:slug])
+   erb :'/subjects/show_subject'
+ end
+
 
 end
