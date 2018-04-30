029d9994dae9b9441a39df9d4908f8d2eb09197a
github username <github email address>
2018-04-30 16:50:08 -0400

    use session to verify methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,45 +14,44 @@ class UsersController < ApplicationController
   end
 
 
-  #Signing Up
-  get '/register' do
-    if !logged_in?
-      erb :'/users/register'
-    else
-      erb :'/users/personal'
-    end
- end
+          #Signing Up
+          get '/register' do
+            if !session[:user_id]?
+              erb :'/users/register'
+            else
+              redirect to '/books'
+            end
+         end
 
          post '/register' do
              if params[:user_name] == "" || params[:email] == "" || params[:password_digest] == ""
                redirect to '/register'
              else
-               @user=User.new(user_name: params[:user_name], email: params[:email], password: params[:password_digest])
-               @user.save
+               @user=User.create(user_name: params[:user_name], email: params[:email], password: params[:password_digest])
                session[:user_id] = @user.id
                redirect to '/books'
              end
          end
 
+
   #Loggin In
  get '/login' do
-   if !logged_in?
+   if !session[:user_id]
      erb :'/users/login'
    else
-     erb :'books/books_list'
+     redirect to '/books'
    end
  end
 
  post '/login' do
-   @user = User.find_by(user_name: params[:user_name])
+   user = User.find_by(user_name: params[:user_name])
        if user && user.authenticate(params[:password])
-       session[:user_id] = @user.id
+       session[:user_id] = user.id
        redirect to '/books'
      else
        redirect to '/login'
      end
  end
-
 
 
  get '/logout' do
@@ -67,16 +66,16 @@ class UsersController < ApplicationController
 
  #list user's books' subject
  get '/users/books/subjects' do
-   if logged_in?
-     @subjects = Subject.all #make subjects controller?
-     erb :'/subjects/subjects'
-   else
+   if !session[:user_id]
      redirect to '/login'
+   else
+     @subjects = Subject.all
+     erb :'/subjects/subjects'
    end
  end
 
  get '/home' do
-   if current_user
+   if session[:user_id]
      erb :'/users/personal'
    else
      erb :'/users/login'
